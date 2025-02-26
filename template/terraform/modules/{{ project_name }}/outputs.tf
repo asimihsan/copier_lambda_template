5d4add2c700316a60dad841fe97f06daac768e06
Asim Ihsan <asim.ihsan@level.co>
2025-02-26 15:26:02 -0500

    wip

--- template/terraform/modules/{{ project_name }}/outputs.tf
+++ template/terraform/modules/{{ project_name }}/outputs.tf
@@ -1,3 +1,11 @@
 # output "tokenissuer_function_arn" {
 #   value = aws_lambda_function.tokenissuer.arn
 # }
+
+output "dynamodb_table_deployment_rotation" {
+  value = aws_dynamodb_table.deployment_rotation.name
+}
+
+output "dynamodb_table_override_requests" {
+  value = aws_dynamodb_table.override_requests.name
+}
