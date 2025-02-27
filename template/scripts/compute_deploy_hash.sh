14bf0daacc576c4391fee3ad626b1074b1b75c24
Asim Ihsan <asim.ihsan@level.co>
2025-02-27 05:57:35 -0500

    wip

---
+++ template/scripts/compute_deploy_hash.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+
+# Get the image hash using the just command
+IMAGE_HASH=$(cd ../../ && just docker-image-hash)
+
+# Output the hash in JSON format for Terraform external data source
+echo "{\"hash\": \"$IMAGE_HASH\"}"
