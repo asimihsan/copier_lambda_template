29ed573da3b4b78a0a04fc4acbd447ad28a9c4e4
Asim Ihsan <asim.ihsan@level.co>
2025-02-27 06:14:06 -0500

    wip

--- template/scripts/compute_deploy_hash.sh
+++ template/scripts/compute_deploy_hash.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-set -e
+set -euo pipefail
+
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+ROOT_DIR=$(realpath "$SCRIPT_DIR/..")
+pushd "$ROOT_DIR" > /dev/null
+trap 'popd > /dev/null' EXIT
 
 # Get the image hash using the just command
-IMAGE_HASH=$(cd ../../ && just docker-image-hash)
+IMAGE_HASH=$(docker inspect --format='{{.Id}}' "$1":latest)
 
 # Output the hash in JSON format for Terraform external data source
 echo "{\"hash\": \"$IMAGE_HASH\"}"
